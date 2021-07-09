198fed5aeb7609ad0027f5a18e3d2e85b883c2db
liguobiao <liguobiao@deepexi.com>
2021-07-09 12:02:33 +0800

    init

---
+++ example/entry.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: liguobiao
+ * @Date: 2021-07-09 11:57:23
+ * @LastEditors: liguobiao
+ * @LastEditTime: 2021-07-09 11:57:24
+ * @FilePath: \my_webpack\example\entry.js
+ */
+console.log(2)
